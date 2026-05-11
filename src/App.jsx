ad8530d0235cbf2b257c4e4c453fdc6e1537a3c8
RocioCanoFernandez <rcanofernandezpiedra@gmail.com>
2026-05-11 14:03:10 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
 
   const vCardData = `BEGIN:VCARD
 VERSION:3.0
-FN:${data.name}
+N:;${data.name};;;\nFN:${data.name}
 ORG:${data.company}
 TITLE:${data.role}
 TEL;TYPE=WORK,VOICE:${data.phoneUrl}
